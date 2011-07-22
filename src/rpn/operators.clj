408d53326cab0e486c335c8a9711495f1670cccd
Zach Raines <raineszm@gmail.com>
2011-07-22 00:29:49 -0400

    Fixed arity bug on -. But the polarity is still reversed. Maybe the system needs to be rethought a bit.

--- src/rpn/operators.clj
+++ src/rpn/operators.clj
@@ -1,13 +1,25 @@
 (ns rpn.operators
   (:use [rpn.stack :only [apply-op]]))
 
-(def *operators* 
-  {
-   :+ {:op + :arity 2}
-   :- {:op - :arity 2}
-   :* {:op * :arity 2}
-   (keyword "/") {:op / :arity 2}})
+(defn binary-op
+  ([op arity]
+   (let [ometa (meta op)
+         kw (keyword (:name ometa))
+         arity (or arity (first (sort (:inline-arities ometa))))]
+     [kw {:op op :arity arity}]))
+  ([op]
+   (if (vector? op)
+     (apply binary-op op)
+     (binary-op op nil))))
 
+(defn construct-ops [& ops]
+  (apply hash-map
+         (mapcat binary-op ops)))
+
+
+;TODO: Fix arity on -
+(def *operators* (construct-ops + [- 2] * /))
+                
 (defn operator? [o]
   (contains? *operators* (keyword o)))
 
