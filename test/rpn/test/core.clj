828461ea4e5fb451a36314660f57d076d40a7cf2
Zach Raines <raineszm@gmail.com>
2011-07-23 22:58:07 -0400

    Wrote some basic integration tests.

--- test/rpn/test/core.clj
+++ test/rpn/test/core.clj
@@ -1,3 +1,15 @@
 (ns rpn.test.core
   (:use [rpn.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:use [clojure.contrib.str-utils :only [chomp]]))
+
+
+(deftest integration
+         (testing "Basic Arithmetic"
+                  (are [line value]
+                       (= (chomp (with-out-str (process-line line))) value)
+                       "1 2 + ." "3.0"
+                       "2 3 * ." "6.0"
+                       "3 2 - ." "1.0"
+                       "6 3 / ." "2.0"
+                       "2 3 ^ ." "8.0")))
