b74bd851e109dd6e27f18dec8a1caace5ba51b60
Zach Raines <raineszm@gmail.com>
2011-07-24 18:31:38 -0400

    Added a bunch of operators and fixed a bug in java-math which made mixed case methods unreachable

--- src/rpn/operators.clj
+++ src/rpn/operators.clj
@@ -30,7 +30,7 @@
             [[arity  a 1]
              [more more other]]
             (let [strname (str name)
-                  aliases (conj (vec more) strname)
+                  aliases (conj (vec more) (.toLowerCase strname))
                   args (repeatedly arity gensym)]
               `[~aliases (fn ~(vec args) (. Math ~name ~@args))
                 ~(java-math-help strname arity) ~arity])))
@@ -39,25 +39,34 @@
 
 (def *operators*
   (construct build-op
-    [:+ + ["x y" "x + y"]]
-    [:- - ["x y" "x - y"] 2]
-    [:* * ["x y" "x * y"]]
-    ["/" / ["x y" "x / y"]]
-    [:neg #(- %) ["x" "-x"] 1]
-    [["^" "**"] math/expt ["x y" "x**y"] 2]
-    [["abs" "||"] math/abs ["x" "|x|"] 1]
-    [["!" "fact"] factorial ["n" "n!"] 1]
-    [["inv" "1/"] #(/ 1 %) ["x" "1/x"] 1]
-    ["sum" + "Sums the contents of the stack" -1]
-    ["prod" * "Multiplies the contents of the stack" -1]
-    (java-math sqrt "v")
-    (java-math sin "s")
-    (java-math cos "c")
-    (java-math tan "t")
-    (java-math asin) 
-    (java-math acos)
-    [:e #(Math/E) "Pushes the constant e to the stack" 1]
-    [:pi #(Math/PI) "Pushes the constant pi to the stack" 1]))
+             [:+ + ["x y" "x + y"]]
+             [:- - ["x y" "x - y"] 2]
+             [:* * ["x y" "x * y"]]
+             ["/" / ["x y" "x / y"]]
+             [:neg #(- %) ["x" "-x"] 1]
+             [["^" "**"] math/expt ["x y" "x**y"] 2]
+             [["abs" "||"] math/abs ["x" "|x|"] 1]
+             [["!" "fact"] factorial ["n" "n!"] 1]
+             [["inv" "1/"] #(/ 1 %) ["x" "1/x"] 1]
+             ["sum" + "Sums the contents of the stack" -1]
+             ["prod" * "Multiplies the contents of the stack" -1]
+             (java-math sqrt "v")
+             (java-math sin "s")
+             (java-math cos "c")
+             (java-math tan "t")
+             (java-math asin) 
+             (java-math acos)
+             (java-math atan)
+             (java-math cosh "ch")
+             (java-math sinh "sh")
+             (java-math tanh "th")
+             (java-math exp)
+             (java-math log "ln")
+             (java-math signum "sgn")
+             (java-math toDegrees "deg")
+             (java-math toRadians "rad")
+             [:e #(Math/E) "Pushes the constant e to the stack" 1]
+             [:pi #(Math/PI) "Pushes the constant pi to the stack" 1]))
                 
 (defn operator? [o]
   (contains? *operators* (keyword o)))
