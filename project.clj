092befdba0b992f6b3ba9cdeaf49a484669c0c44
Zach Raines <rainezm@gmail.com>
2011-08-02 21:22:51 -0400

    Removed dev dependencies. They should be plugins anyway.

--- project.clj
+++ project.clj
@@ -2,6 +2,4 @@
   :description "A simple RPN calculator"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[vimclojure/server "2.2.0"]
-                     [org.clojars.autre/lein-vimclojure "1.0.0"]]
   :main cljrpn.core)
