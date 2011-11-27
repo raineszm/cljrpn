cbd2c7ead117ffa0092fb1c60b22a4aa61a7bb97
Zach Raines <raineszm@gmail.com>
2011-11-27 12:29:40 -0500

    Added midje and will start converting tests.

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "A simple RPN calculator"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.0"]
-                 [org.clojure/math.numeric-tower "0.0.1"]]
+                 [org.clojure/math.numeric-tower "0.0.1"]
+                 [midje "1.3-alpha5"]]
   :main cljrpn.core)
