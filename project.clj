64d0189845b7c1d07da7281a710138453ea5830c
Zach Raines <raineszm@gmail.com>
2013-10-03 17:14:02 -0400

    Updating for Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject cljrpn "0.1.0"
   :description "A simple RPN calculator"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.cli "0.2.1"]
-                 [org.clojure/math.numeric-tower "0.0.1"]
-                 [midje "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [org.clojure/math.numeric-tower "0.0.2"]
+                 [midje "1.6-beta1"]]
+  :plugins [[lein-midje "3.1.2"]]
   :checksum-deps true
   :main cljrpn.core)
