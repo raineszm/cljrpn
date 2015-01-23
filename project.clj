7a840776fb074b03cf0c4e9e3a6ec1a15c97c736
Zach Raines <raineszm@gmail.com>
2015-01-22 20:03:27 -0500

    Updating plugin versions.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject cljrpn "0.1.0"
   :description "A simple RPN calculator"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.2.4"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
-                 [midje "1.6-beta1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
   :checksum-deps true
   :main cljrpn.core
   :aot [cljrpn.core])
