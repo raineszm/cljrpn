31531009677d7b9236fad94e74e2d988974c5260
Zach Raines <raineszm@gmail.com>
2015-01-28 10:46:57 -0500

    Clean up state tests a little.

--- test/cljrpn/test/state.clj
+++ test/cljrpn/test/state.clj
@@ -4,12 +4,12 @@
   (:use midje.sweet))
 
 (fact "Pushf a should add to the top of the stack"
-  (pushf t-empty-state 1)=> (contains {:stack '(1)}))
+  (pushf t-empty-state 1)=> (stack-t '(1)))
 
 (unfinished g)
 
 (fact "update-stack replace the stack"
-  (update-stack (new-state '()) g) => (contains {:stack '(4)})
+  (update-stack (new-state ..stack..) g) => (stack-t '(4))
   (provided
     (g anything) => '(4)))
 
@@ -17,4 +17,4 @@
   (top (new-state '(1 2 3))) => 1)
 
 (fact "popf removes the top of the stack"
-  (popf (new-state '(1 2 3))) => (contains {:stack '(2 3)}))
+  (popf (new-state '(1 2 3))) => (stack-t '(2 3)))
