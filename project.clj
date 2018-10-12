83658a551816281f98bf07bb0b532b998ffba0c2
Zach Raines <raineszm@gmail.com>
2018-10-12 15:55:06 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject cljrpn "0.1.0"
   :description "A simple RPN calculator"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.cli "0.4.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/tools.reader "0.8.13"]]
+                 [org.clojure/tools.reader "1.3.0"]]
   :resource-paths ["resources/"]
   :profiles {:uberjar {:aot :all},
-             :dev {:dependencies [[midje "1.6.3"]]}}
+             :dev {:dependencies [[midje "1.9.3"]]}}
   :checksum-deps true
   :main cljrpn.core)
