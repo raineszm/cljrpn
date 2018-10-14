d88d29cb1a089dc147f5d9a043e5e64872077e8d
Zach Raines <raineszm@gmail.com>
2018-10-13 22:39:29 -0400

    Get tests running

--- test/cljrpn/test/modifiers.clj
+++ test/cljrpn/test/modifiers.clj
@@ -5,14 +5,13 @@
         cljrpn.test.helpers
         midje.sweet))
 
-(def t-state-w-registers
-  (set-register
-    t-empty-state
-    "a"
-    ..val..))
-
 (fact "The store modifier places to top value of the stack into the specified register"
       (store (new-state '(..val..)) "a") => (register-t :a ..val..))
 
 (fact "The retrieve modifer pushes the requested register to the stack."
-      (retrieve t-state-w-registers "a") => (stack-t '(..val..)))
+      (retrieve
+        (set-register
+          t-empty-state
+          "a"
+          ..val..)
+        "a") => (stack-t '(..val..)))
